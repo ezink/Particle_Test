1ea6262b6d55ff7b5413ed0f391e21b819140d22
ezink <ethaneze77@gmail.com>
2015-08-08 13:45:01 -0600

    Finals
    Finals

---
+++ Particle-Test/Assets/SlowMotion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowMotion : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			if (Time.timeScale == 1.0F){
+				Time.timeScale = 0.1F;
+				Debug.Log("SLOWMOTION");
+			}
+			//else
+				//Time.timeScale = 1.0F;
+			Time.fixedDeltaTime = 0.02F * Time.timeScale;
+		}
+	}
+}
+
