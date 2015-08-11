c9c926bd49c171f5f07b39d8cf5a6453fd688af8
ezink <ethaneze77@gmail.com>
2015-08-10 22:36:02 -0600

    After Demo Reel
    After Demo Reel

--- Particle-Test/Assets/SlowMotion.cs
+++ Particle-Test/Assets/SlowMotion.cs
@@ -3,15 +3,35 @@ using System.Collections;
 
 public class SlowMotion : MonoBehaviour {
 
+	public ParticleSystem frost;
+	public ParticleSystem lightning;
+	public Animator cameraControl;
+
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.A)) {
 			if (Time.timeScale == 1.0F){
 				Time.timeScale = 0.1F;
 				Debug.Log("SLOWMOTION");
 			}
-			//else
-				//Time.timeScale = 1.0F;
 			Time.fixedDeltaTime = 0.02F * Time.timeScale;
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			frost.Play ();
+			Debug.Log ("FROST");
+		}
+
+		if (Input.GetKeyDown (KeyCode.L)) {
+			lightning.Play();
+			Debug.Log ("LIGHTNING");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			cameraControl.enabled = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.X)) {
+			cameraControl.enabled = false;
 		}
 	}
 }
